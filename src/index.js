b18729b13e90428651814d397458dc89fb95391f
Iran Adryan <contatoiranadryan@gmail.com>
2023-04-05 10:01:41 -0300

    fisrt commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+const wss = new WebSocketServer({
+  port: PORT
+});
+
+let sockets = [];
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+
+  socket.on('close', () => {
+    sockets = sockets.filter((s) => s !== socket);
+  })
+});
+
+app.post('/hook', (req, res) => {
+  console.log(req.body);
+  sockets.forEach((socket) => {
+    socket.send(req.body);
+  })
+  
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
