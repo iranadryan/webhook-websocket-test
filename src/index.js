03ca1cd466c16b72dca4a8be90947a113bdf1f97
Iran Adryan <contatoiranadryan@gmail.com>
2023-04-05 10:07:58 -0300

    fix: change ws port

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { WebSocketServer } from 'ws';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const wss = new WebSocketServer({
-  port: PORT
+  port: 8080,
 });
 
 let sockets = [];
